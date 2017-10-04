e39dc6371286b6eafa058eb21a42f9fc44a2f501
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-04 04:12:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function happyHolidaysTo(name){
   return 'Happy holidays ' + name + '!';
 }
 
-function happyHolidaysTo(name, holiday){
+function happyHolidayTo(name, holiday){
   return 'Happy ' + holiday + ',' + name +'!';
 }
 
