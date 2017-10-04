e605eafac10d4d7992fadf7068bc301e35c832ac
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-04 03:59:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+var string = hotelHello();
+
+function hotelHello(){
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name){
+  return 'Happy holidays ${name}!';
+}
