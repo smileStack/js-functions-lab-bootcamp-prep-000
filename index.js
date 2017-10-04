f021174ab16bbb5ea1a8026809e20a679558c10a
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-04 04:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // write your code below!
 var string = happyHolidays();
 var name = "Joe";
+var holiday = "July4th"
 
 function happyHolidays(){
   return "Happy holidays!";
 }
 
 function happyHolidaysTo(name){
-  return 'Happy holidays ${name}!';
+  return 'Happy holidays' + ${name}!;
+}
+
+function happyHolidaysTo(name, holiday){
+  return 'Happy ${holiday}, ${name}!'
+}
+
+function holidayCountdown(days){
+  return `It's ${days} days until ${holiday}!`;
 }
